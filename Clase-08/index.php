bceb8243238cc9297e75a8c78023e24ea1372902
Your Name <you@example.com>
2019-10-07 21:05:53 -0300

    group

--- Clase-08/index.php
+++ Clase-08/index.php
@@ -9,9 +9,33 @@ $config ['addContentLengthHeader'] = false;
 
 $app = new \Slim\App(["settings" => $config]);
 
-$app->get('/hello', function ($request,  $response, $args) {
-    $response->getBody()->write("Hello, World");
-    return $response;
+// $app->get('/hello', function ($request,  $response, $args) {
+//     $response->getBody()->write("Hello, World");
+//     return $response;
+
+$app->group('/saludo', function() {
+    $this->get('/', function ($request, $response, $args) {
+        $response->getbody()->write("Hello world get");
+    });
+
+    $this->get('/{nombre}', function ($request, $response, $args) {
+        $nombre=$args['nombre'];
+        $response->getbody()->write("Hello <h1>$nombre</h1>");
+    });
+
+    $this->post('/', function ($request, $response, $args) {
+        $response->getbody()->write("Hello world post");
+    });
+
+    $this->put('/', function ($request, $response, $args) {
+        $response->getbody()->write("Hello world put");
+    });
+
+    $this->delete('/', function ($request, $response, $args) {
+        $response->getbody()->write("Hello world delete");
+    });
+
+
 });
 
 $app -> run();
