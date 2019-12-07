b6564ba7b248ab580a4a3042ef4f2d31d612fee2
gabrielivan <gabrielivansaliba@gmail.com>
2019-12-07 18:55:04 -0300

    updated-ver-pendientes

---
+++ TP_COMANDA/src/app/modelORM/mesaController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Models\ORM;
+
+use App\Models\AutentificadorJWT;
+use App\Models\ORM\Mesa;
+
+
+include_once __DIR__ . '/mesa.php';
+include_once __DIR__ . '../../modelAPI/AutentificadorJWT.php';
+
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+class mesaController
+{
+    public function traerMesas($request, $response, $args)
+    {
+        $todasLasMesas = Mesa::all();
+        if(count($todasLasMesas) > 0) 
+        {
+            $newResponse = $response->withJson($todasLasMesas, 200);
+        }
+        else{
+            $newResponse = $response->withJson("No hay mesas", 200);
+        }
+        return $newResponse;
+    }
+    public function obtenerMesaLibre()
+    {
+        $mesaLibre = Mesa::where("idEstadoMesa", "=", "4")
+        ->select("codigoMesa")
+        ->first();
+        
+        if($mesaLibre != null)
+        {
+            $newResponse = $mesaLibre->codigoMesa;
+            self::cambiarEstado($mesaLibre->codigoMesa, 1);
+        }
+        else
+        {
+            $newResponse = null;
+        }
+        return $newResponse;
+    }
+    public function cambiarEstado($codigoMesa, $nuevoEstado){
+        $mesa=mesa::where('codigoMesa','=',$codigoMesa)->first();
+        $mesa->idEstadoMesa=$nuevoEstado;
+        $mesa->save();
+    }
+    public function traerUnaMesa($request, $response, $args)
+    {
+        $id = $args["id"];
+        $mesa = Mesa::find($id);
+        if(count((array)$mesa) > 0) 
+        {
+            $newResponse = $response->withJson($mesa, 200);
+        }
+        else{
+            $newResponse = $response->withJson("No hay mesa con ese ID", 200);
+        }
+        return $newResponse;
+    }
+
+    public function altaMesa($request, $response, $args)
+    {
+        $mesa = new Mesa;
+        $mesa->codigoMesa = " ";
+        $mesa->idEstadoMesa = 4;
+        $mesa->save();
+        $mesa->codigoMesa = "MESA-".$mesa->id;
+        $mesa->save();
+        $newResponse = $response->withJson('Mesa ' . $mesa->codigoMesa. ' cargada', 200);
+        return $newResponse;
+    }
+    public function bajaMesa($request, $response, $args)
+    {
+        $parametros = $request->getParsedBody();
+        $id = $parametros['id'];
+        $mesaEncontrada = Mesa::find($id);
+        
+        if($mesaEncontrada != null)
+        {
+            $mesaEncontrada->delete();
+            $newResponse = $response->withJson('La Mesa: ' . $id . ' fue dada de baja con existo.', 200);
+        }
+        else{
+            $newResponse = $response->withJson('La Mesa que quiere borrar no existe', 200);
+        }
+        return $newResponse;
+    }
+
+    public function modificarMesa($request, $response, $args)
+    {
+        $arrayDeParametros = $request->getParsedBody();
+        $id = null;
+        $mesa = null;
+        $contador = 0;
+        
+        if (array_key_exists("id", $arrayDeParametros)) {
+            $id = $arrayDeParametros['id'];
+            $mesa = Mesa::find($id); //obtuve la mesa.
+        }
+        if($id != null && $mesa != null){ //pregunto si mandaron bien el id y si se encontro la mesa.
+            
+            if (array_key_exists("codigoMesa", $arrayDeParametros)) {
+                $mesa->codigoMesa = $arrayDeParametros["codigoMesa"];
+                $contador++;
+            }
+            if (array_key_exists("idEstadoMesa", $arrayDeParametros)) {
+                $mesa->idEstadoMesa = $arrayDeParametros["idEstadoMesa"];
+                $contador++;
+            }
+            if ($contador > 0 && $contador <= 2) {
+                $mesa->save();
+                $newResponse = $response->withJson('Se ha modificado la mesa: ' . $mesa->codigoMesa . ' con existo.', 200);
+            } 
+        }
+        else if ($id == null) {
+            $newResponse = $response->withJson('Introduzca un id valido', 200);
+          } 
+          else if ($id != null && $mesa == null) {
+            $newResponse = $response->withJson("No hay una mesa con el id ingresado", 200);
+          } 
+          else {
+            $newResponse = $response->withJson("No hubo modificaciones ", 200);
+          }
+        return $newResponse;
+    }
+}
---
+++ TP_COMANDA/src/routes/routesPedidos.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\ORM\pedido_productoController;
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use App\Models\ORM\pedidoController;
+
+include_once __DIR__ . '/../../src/app/modelORM/pedidoController.php';
+include_once __DIR__ . '/../../src/app/modelORM/pedido_productoController.php';
+return function (App $app) {
+
+
+    $app->group('/pedidos', function () {
+
+        $this->post('/altaPedido', pedidoController::class . ':altaPedido')->add(Middleware::class . ":EsMozo")
+                                                                           ->add(Middleware::class . ":validarToken");
+
+        $this->post('/bajaPedido', pedidoController::class . ':bajaPedido')->add(Middleware::class . ":EsMozo")
+                                                                           ->add(Middleware::class . ":validarToken");
+        
+        $this->post('/modificarPedido', pedidoController::class . ':modificarPedido')->add(Middleware::class . ":EsMozo")
+                                                                                     ->add(Middleware::class . ":validarToken");
+        
+        $this->get('/traerPedidos', pedidoController::class . ':traerPedidos');
+        
+        $this->get('/traerUnPedido', pedidoController::class . ':traerUnPedido');
+
+        $this->post('/prepararPedido', pedidoController::class . ':prepararPedido') ->add(Middleware::class . ":validarToken");
+
+        $this->post('/terminarPedido', pedidoController::class . ':terminarPedido') ->add(Middleware::class . ":validarToken");
+
+        $this->post('/servirPedido', pedidoController::class . ':servirPedido')->add(Middleware::class . ":EsMozo")
+                                                                               ->add(Middleware::class . ":validarToken");
+        
+        $this->get('/pedirCuenta', pedidoController::class . ':pedirCuenta')->add(Middleware::class . ":EsMozo")
+                                                                            ->add(Middleware::class . ":validarToken");
+        
+        $this->get('/cobrarPedido', pedidoController::class . ':cobrarPedido')->add(Middleware::class . ":EsMozo")
+                                                                              ->add(Middleware::class . ":validarToken");
+        
+        $this->get('/verPendientes', pedido_productoController::class . ':verPedidosPendientes')->add(Middleware::class . ":validarToken");
+
+    });
+};
